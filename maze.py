57d0242c34a3a22ad3495c63a95b2ca8c0a9c01b
Victor Restrepo <37878478+RestrepoTorres@users.noreply.github.com>
2021-01-10 14:56:40 -0500

    se eliminan correctamente los caminos ciegos

--- maze.py
+++ maze.py
@@ -1,76 +1,84 @@
+import random
+
 xInicial = 1
 yInicial = 1
-xFinal = 8
-yFinal = 8
+xFinal = 10
+yFinal = 10
+maze = []
 xActual = xInicial
 yActual = yInicial
-xAnterior = xInicial
-yAnterior = yInicial
+cantidadDeCeros = 0
 caminoSeguido = []
-
-bifurcaciones = []
 caminosExitosos = []
-todoExplorado = False
-
-maze = []
 
 
 def cargar_laberinto():
-    global maze
+    global maze, cantidadDeCeros
     file = open("laberinto1.txt")
     lines = file.readlines()
     maze = []
-    # maze2 = []
     row = []
     for line in lines:
         for char in line:
             if char in "10":
                 row.append(int(char))
         maze.append(row)
-        # maze2.append(row)
         row = []
+    cantidadDeCeros = contar_ceros()
+    sellar_camino_ciego()
+
+
+def contar_ceros():
+    global maze
+    i = 0
+    for x in range(12):
+        for y in range(12):
+            if maze[x][y] == 0:
+                i = i + 1
+    return i
+
+
+def es_camino_ciego(x, y):
+    global maze, xInicial, yInicial, xFinal, yFinal
+    if (x == xInicial and y == yInicial) or (x == xFinal and y == yFinal):
+        return False
+    else:
+        return suma_opciones(x, y) >= 3
+
+
+def suma_opciones(x, y):
+    global maze
+    return maze[x][y + 1] + maze[x][y - 1] + maze[x + 1][y] + maze[x - 1][y]
+
+
+def sellar_camino_ciego():
+    global maze, cantidadDeCeros
+    i = 0
+    while i <= cantidadDeCeros:
+        for x in range(1, 11):
+            for y in range(1, 11):
+                if es_camino_ciego(x, y):
+                    maze[x][y] = 1
+        i += 1
 
 
 def avanzar():
-    global xActual, yActual, xAnterior, yAnterior
-
+    global xActual, yActual
+    x = random.randint(1, 4)
     # derecha
-    if camino_despejado(0, 1) and no_devolverse(0, 1):
-        yAnterior = yActual
-        xAnterior = xActual
+    if camino_despejado(0, 1) and x == 1:
         yActual = yActual + 1
     # abajo
-    elif camino_despejado(1, 0) and no_devolverse(1, 0):
-        yAnterior = yActual
-        xAnterior = xActual
+    elif camino_despejado(1, 0) and x == 2:
         xActual = xActual + 1
     # izquierda
-    elif camino_despejado(0, -1) and no_devolverse(0, -1):
-        yAnterior = yActual
-        xAnterior = xActual
+    elif camino_despejado(0, -1) and x == 3:
         yActual = yActual - 1
     # arriba
-    elif camino_despejado(-1, 0) and no_devolverse(-1, 0):
-        yAnterior = yActual
-        xAnterior = xActual
+    elif camino_despejado(-1, 0) and x == 4:
         xActual = xActual - 1
     else:
         print("algo se hizo mal")
-
-
-def es_bifurcacion():
-    return suma_opciones() < 2
-
-
-def es_camino_ciego():
-    return suma_opciones() == 3
-
-
-def suma_opciones():
-    global xActual, yActual, maze
-    x = xActual
-    y = yActual
-    return maze[x][y + 1] + maze[x - 1][y] + maze[x][y - 1] + maze[x + 1][y]
 
 
 def camino_despejado(x, y):
@@ -78,82 +86,12 @@ def camino_despejado(x, y):
     return maze[xActual + x][yActual + y] == 0
 
 
-def no_devolverse(x, y):
-    global xActual, yActual, xAnterior, yAnterior
-    return not (((xActual + x) == xAnterior) and ((yActual + y) == yAnterior))
-
-
-def desapilar_por_camino_ciego():
-    global xInicial, yInicial, xActual, yActual, xAnterior, yAnterior, caminoSeguido, todoExplorado
-    a, b, c, ww, www = -1, -1, False, -1, -1
-
-    while not c:
-        a, b, c, ww, www = caminoSeguido.pop()
-        if a == xInicial and b == yInicial:
-            xActual = xInicial
-            yActual = yInicial
-            xAnterior = xInicial
-            yAnterior = yInicial
-            sellar_camino_ciego()
-            if suma_opciones() == 4:
-                todoExplorado = True
-        elif c:
-            # TODO: simplificar este método poniendo parte en sellar
-            xActual = a
-            yActual = b
-            xAnterior = ww
-            yAnterior = www
-            sellar_camino_ciego()
-            xAnterior = ww
-            yAnterior = www
-            caminoSeguido.append((a, b, es_bifurcacion(), ww, www))
-
-
-def sellar_camino_ciego():
-    global maze, xActual, xInicial, yActual, yInicial, xAnterior, yAnterior
-    x = xActual
-    y = yActual
-    xa = xAnterior
-    ya = yAnterior
-    avanzar()
-    maze[xActual][yActual] = 1
-    xActual = x
-    yActual = y
-    xAnterior = xa
-    yAnterior = ya
-
-
-
 # A PARTIR DE AQUÍ EJECUCIÓN DEL PROGRAMA
 cargar_laberinto()
+
 caminoSeguido.append((xInicial, yInicial, True, xInicial, yInicial))
 
-while not todoExplorado:
-
-    print(" X actual: " + str(xActual) + ". Y actual: " + str(yActual))
-    print(" X anterior: " + str(xAnterior) + ". Y anterior: " + str(yAnterior))
-    avanzar()
-
-    """if (xActual)
-        
-    else:
-        xActual, yActual = caminoSeguido[-1][0], caminoSeguido[-1][1]"""
-
-    if xActual == xFinal and yActual == yFinal:
-        caminosExitosos.append(str(caminoSeguido))
-        desapilar_por_camino_ciego()
-        print("X actual: " + str(xActual) + ".Y actual: " + str(yActual))
-        print("final encontrado")
-        continue
-    elif es_camino_ciego():
-        desapilar_por_camino_ciego()
-
-    caminoSeguido.append((xActual, yActual, es_bifurcacion(), xAnterior, yAnterior))
-    # for i in range(8):
-    #    print(i, maze[i][:])
-print("los caminos hayados al final fueron los siguientes: ")
-if len(caminosExitosos) == 0:
-    print("no hay soluciones para mostrar")
-else:
-    for element in caminosExitosos:
-        print(element)
+# DEBUG PRINTS
+# print(cantidadDeCeros)
+# for element in maze:
+#    print(element)
