6de72968c1f71f0bfa34cdcd58dd3b97348313ca
Victor Restrepo <37878478+RestrepoTorres@users.noreply.github.com>
2021-01-10 21:11:15 -0500

    todo lo relacionado con avanzar y almacenar opciones esta hecho, funciona sin problemas hasta ahora

--- maze.py
+++ maze.py
@@ -26,72 +26,114 @@ def cargar_laberinto():
         row = []
     cantidadDeCeros = contar_ceros()
     sellar_camino_ciego()
+    cantidadDeCeros = contar_ceros()
 
 
 def contar_ceros():
     global maze
     i = 0
-    for x in range(12):
-        for y in range(12):
-            if maze[x][y] == 0:
+    for m in range(12):
+        for n in range(12):
+            if maze[m][n] == 0:
                 i = i + 1
     return i
 
 
-def es_camino_ciego(x, y):
+def es_camino_ciego(xx, yy):
     global maze, xInicial, yInicial, xFinal, yFinal
-    if (x == xInicial and y == yInicial) or (x == xFinal and y == yFinal):
+    if (xx == xInicial and yy == yInicial) or (xx == xFinal and yy == yFinal):
         return False
     else:
-        return suma_opciones(x, y) >= 3
+        return suma_opciones(xx, yy) >= 3
 
 
-def suma_opciones(x, y):
+def suma_opciones(x1, y1):
     global maze
-    return maze[x][y + 1] + maze[x][y - 1] + maze[x + 1][y] + maze[x - 1][y]
+    return maze[x1][y1 + 1] + maze[x1][y1 - 1] + maze[x1 + 1][y1] + maze[x1 - 1][y1]
 
 
 def sellar_camino_ciego():
     global maze, cantidadDeCeros
     i = 0
     while i <= cantidadDeCeros:
-        for x in range(1, 11):
+        for x4 in range(1, 11):
             for y in range(1, 11):
-                if es_camino_ciego(x, y):
-                    maze[x][y] = 1
+                if es_camino_ciego(x4, y):
+                    maze[x4][y] = 1
         i += 1
 
 
 def avanzar():
-    global xActual, yActual
-    x = random.randint(1, 4)
+    global xActual, yActual, xInicial, yInicial, xFinal, yFinal
+    caminoSeguido.append((xActual, yActual))
+    w = random.randint(1, 4)
     # derecha
-    if camino_despejado(0, 1) and x == 1:
+    if camino_despejado(0, 1) and w == 1:
+        xActual = xActual
         yActual = yActual + 1
     # abajo
-    elif camino_despejado(1, 0) and x == 2:
+    elif camino_despejado(1, 0) and w == 2:
+        yActual = yActual
         xActual = xActual + 1
     # izquierda
-    elif camino_despejado(0, -1) and x == 3:
+    elif camino_despejado(0, -1) and w == 3:
+        xActual = xActual
         yActual = yActual - 1
     # arriba
-    elif camino_despejado(-1, 0) and x == 4:
+    elif camino_despejado(-1, 0) and w == 4:
+        yActual = yActual
         xActual = xActual - 1
     else:
-        print("algo se hizo mal")
+        if camino_despejado(0, 1):
+            xActual = xActual
+            yActual = yActual + 1
+        elif camino_despejado(1, 0):
+            yActual = yActual
+            xActual = xActual + 1
+        elif camino_despejado(0, -1):
+            xActual = xActual
+            yActual = yActual - 1
+        elif camino_despejado(-1, 0):
+            yActual = yActual
+            xActual = xActual - 1
 
 
-def camino_despejado(x, y):
+def camino_despejado(x2, y2):
     global maze, xActual, yActual
-    return maze[xActual + x][yActual + y] == 0
+    return maze[xActual + x2][yActual + y2] == 0
 
 
 # A PARTIR DE AQUÍ EJECUCIÓN DEL PROGRAMA
 cargar_laberinto()
+for x in range(100000):
+    avanzar()
+    w = caminoSeguido.copy()
+    w.pop()
+    if (xActual, yActual) in w:
+        xActual = xInicial
+        yActual = yInicial
+        caminoSeguido.clear()
+        w.clear()
+    if (xActual == xFinal) and (yActual == yFinal):
+        caminoSeguido.append((xActual, yActual))
+        s = caminoSeguido.copy()
 
-caminoSeguido.append((xInicial, yInicial, True, xInicial, yInicial))
+        if len(s) <= cantidadDeCeros:
+            caminosExitosos.append(str([s]))
+        caminoSeguido.clear()
+        xActual = xInicial
+        yActual = yInicial
 
+print(len(caminosExitosos))
+newlist = sorted(set(caminosExitosos), key=lambda x: caminosExitosos.index(x))
+
+for element in newlist:
+    print(element)
+
+print(len(newlist))
 # DEBUG PRINTS
+# print(caminosExitosos.pop())
+# print(len(caminosExitosos))
 # print(cantidadDeCeros)
 # for element in maze:
-#    print(element)
+# print(element)
