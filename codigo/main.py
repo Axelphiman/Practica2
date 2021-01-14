1d5f3f48b818993f635c5abf31e50011004c8cdd
RestrepoTorres <vrtestrepo@gmail.com>
2021-01-14 13:29:36 -0500

    small change

--- codigo/main.py
+++ codigo/main.py
@@ -15,18 +15,18 @@ columnas = -1
 
 
 def cargar_laberinto():
-    """Almacena el laberinto en una matriz
+    """Almacena el laberinto en una matriz.
 
-    Función:
+    :Función:
         Leer las lineas del archivo seleccionado (ver sección  "Jerarquia
         de archivos") para conformar las filas de la matriz y colummnas de la
-        matriz
+        matriz.
 
-    Modifica:
-        matriz_laberinto: le agrega los unos y ceros que conforman el laberinto
+    :Modifica:
+        matriz_laberinto: le agrega los unos y ceros que conforman el laberinto.
 
     """
-    global matriz_laberinto, filas, columnas
+    global matriz_laberinto, filas, columnas, x_final, y_final
     archivo = open("../laberintos/laberinto1.txt")
     archivo_completo = archivo.readlines()
     matriz_laberinto = []
@@ -43,24 +43,22 @@ def cargar_laberinto():
     sellar_camino_cerrado()
     x_final = filas - 2
     y_final = columnas - 2
-    print(x_final, y_final)
 
 
-def es_camino_cerrado(x_parametro, y_parametro):
-    """Compueba si una celda es o no un camino sin salida
+def es_camino_cerrado(x_parametro: int, y_parametro: int) -> bool:
+    """Compueba si una celda es o no un camino sin salida.
 
-    Función:
+    :Función:
         Servir como método de control para apoyar las
-        decisiones tomadas en otros métodos
+        decisiones tomadas en otros métodos.
 
-    Argumentos:
-        x_parametro (int): coordenada x de la casilla a verificar
+    Args:
+        x_parametro (int): coordenada x de la casilla a verificar.
+        y_parametro (int): coordenada y de la casilla a verificar.
 
-        y_parametro (int): coordenada y de la casilla a verificar
-
-    Retorno:
+    :Retorno:
         Bool: True en caso de que la casilla sea un camino sin salida, falso
-        de lo contrario
+        de lo contrario.
 
     """
     global matriz_laberinto, x_inicial, y_inicial, x_final, y_final
@@ -73,19 +71,18 @@ def es_camino_cerrado(x_parametro, y_parametro):
         return suma_adyacentes(x_parametro, y_parametro) >= 3
 
 
-def suma_adyacentes(x_parametro, y_parametro):
-    """Suma los valores de las casillas adyacentes
+def suma_adyacentes(x_parametro: int, y_parametro: int) -> int:
+    """Suma los valores de las casillas adyacentes.
 
         Función: método auxiliar para el funcionamiento de otros métodos que requieren
-        comprobar el estado de una casilla
+        comprobar el estado de una casilla.
 
-        Argumentos:
-            x_parametro: coordenada x de la casilla a verificar
-
-            y_parametro: coordenada y de la casilla a verificar
+        Args:
+            x_parametro: coordenada x de la casilla a verificar.
+            y_parametro: coordenada y de la casilla a verificar.
 
         Retorno:
-            int: suma del valos de las casilla adyacentes
+            int: suma del valos de las casilla adyacentes.
     """
     global matriz_laberinto
     return (matriz_laberinto[x_parametro][y_parametro + 1] +
@@ -95,20 +92,19 @@ def suma_adyacentes(x_parametro, y_parametro):
 
 
 def sellar_camino_cerrado():
-    """Tapa con unos (1) los caminos cerrados
+    """Tapa con unos (1) los caminos cerrados.
 
-        Función:
+        :Función:
             simplificar el laberinto para evitar avanzar por
-            caminos sin salida
+            caminos sin salida.
 
-        Modifica:
-            matriz_laberintos: cambia el valor de las celdas que son camino cerrado
+        :Modifica:
+            matriz_laberintos: cambia el valor de las celdas que son parte
+             de un camino cerrado.
 
     """
     global matriz_laberinto, filas, columnas
-    print(filas)
-    print(columnas)
-    for w in range(filas + columnas):
+    for i in range(filas + columnas):
         for x4 in range(0, filas - 1):
             for y in range(0, columnas - 1):
                 if es_camino_cerrado(x4, y):
@@ -116,17 +112,17 @@ def sellar_camino_cerrado():
 
 
 def avanzar():
-    """Avanza de una casilla a una subsiguiente
-        Función:
-            avanzar por la matriz actualizando las coordenadas actuales
-            y comprobando que casillas estan disponibles para el avance
+    """Avanza de una casilla a una subsiguiente.
 
-        Modifica:
-            x_actual: le resta o suma uno(1) a la coordenada x actual
-            y_actual: le resta o suma uno(1) a la coordenada y actual
+        :Función:
+            avanzar por la matriz actualizando las coordenadas actuales
+            y comprobando que casillas estan disponibles para el avance.
+
+        :Modifica:
+            x_actual: le resta o suma uno (1).
+            y_actual: le resta o suma uno (1).
 
     """
-
     global x_actual, y_actual, camino_seguido
     camino_seguido.append((x_actual, y_actual, es_bifurcacion()))
     bifurcaciones.append(es_bifurcacion())
@@ -159,19 +155,18 @@ def avanzar():
     add_a_finales()
 
 
-def camino_disponible(x_parametro, y_parametro):
-    """Controla por cuales casillas adyacentes se puede avanzar
+def camino_disponible(x_parametro: int, y_parametro: int) -> bool:
+    """Controla por cuales casillas adyacentes se puede avanzar.
 
-        Función:
-            servir como método de apoyo para las toma de decisiones en avanzar()
+        :Función:
+            servir como método de apoyo para las toma de decisiones en avanzar().
 
-        Argumentos:
-            x_parametro (int): valor a sumar en la coordenada x
+        Args:
+            x_parametro (int): valor a sumar en la coordenada x.
+            y_parametro (int): valor a sumar en la cordenada y.
 
-            y_parametro (int): valor a sumar en la cordenada y
-
-        Retorno:
-            Bool: True si es posible avanzar por esa casilla, False de lo contrario
+        :Retorno:
+            Bool: True si es posible avanzar por esa casilla, False de lo contrario.
 
     """
     global matriz_laberinto, x_actual, y_actual
@@ -179,27 +174,22 @@ def camino_disponible(x_parametro, y_parametro):
 
 
 def eliminar_caminos_repetidos():
-    """Elimina soluciones al laberinto duplicadas
+    """Elimina soluciones al laberinto duplicadas.
     """
     global caminos_exitosos
     caminos_exitosos = sorted(set(caminos_exitosos), key=lambda k: caminos_exitosos.index(k))
 
 
-def es_bifurcacion():
-    """Compueba si una celda es o no un camino una bifurcación del camino
+def es_bifurcacion() -> bool:
+    """Compueba si la celda actual es o no un camino una bifurcación del camino.
 
-    Función:
+    :Función:
         Servir como método de control para apoyar las
         decisiones tomadas en otros métodos
 
-    Argumentos:
-        x_parametro (int): coordenada x de la casilla a verificar
-
-        y_parametro (int): coordenada y de la casilla a verificar
-
-    Retorno:
+    :Retorno:
         Bool: True en caso de que la casilla sea bifurcacion, falso
-        de lo contrario
+        de lo contrario.
 
     """
     if (x_actual == x_inicial) and (y_actual == y_inicial):
@@ -216,15 +206,25 @@ def mostrar_soluciones():
         print("ingrese el número de la que desea ver, ingrese XXX para ver la mejor, ingrese WWWW"
               + " para verlas todas")
         eleccion = int(input())
-        for x in range(eleccion - 1):
+        for i in range(eleccion - 1):
             copia.pop
         print(copia.pop())
 
 
-def no_repetir():
-    """Evita que se pase dos veces por encima del mismo camino
+def no_repetir() -> bool:
+    """Evita que se pase dos veces por encima del mismo camino.
 
+        :Función:
+            verificar en la pila del camino seguido si ya se
+            habia estado anteriormente en esa casilla.
 
+        :Modifica:
+            camino_seguido: le añade las coordenadas análizadas
+            en caso de que sean totalmente nuevas.
+
+        :Retorna:
+            Bool: True si en las coordenadas ya se había
+            visidado con anterioridad, False de lo contario.
 
     """
     global x_actual, y_actual, camino_seguido, bifurcaciones, matriz_laberinto
@@ -242,6 +242,16 @@ def no_repetir():
 
 
 def add_a_finales():
+    """Almacena caminos a la pila de finales
+
+        :Función:
+            Si un camino logra partir desde el inicio y llegar hasta el final,
+             este método lo almacena
+
+        :Modifica:
+            caminos_exitosos: le  añade un nuevo camino a la pila.
+
+    """
     global x_actual, y_actual, camino_seguido, caminos_exitosos
     if (x_actual == x_final) and (y_actual == y_final):
         camino_seguido.append((x_actual, y_actual))
@@ -257,6 +267,4 @@ cargar_laberinto()
 for x in range(500000):
     avanzar()
 
-print(len(caminos_exitosos))
 eliminar_caminos_repetidos()
-print(len(caminos_exitosos))
--- docs/_sources/delimitacion_solucion.rst.txt
+++
@@ -1,3 +0,0 @@
-Delimitación de la solución
-===========================
-
